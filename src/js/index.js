79b2696fe313a0afb7ccc43072cb270d7fbab960
Vicente Monreal <monre036@gmail.com>
2024-05-15 20:31:53 +0200

    changes 6

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,15 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
 import '../scss/styles.scss';
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menuIcon = document.getElementById('menu-icon');
+    const dropdownMenu = document.getElementById('dropdown-menu');
+
+    menuIcon.addEventListener('click', function () {
+        if (dropdownMenu.style.display === 'none' || dropdownMenu.style.display === '') {
+            dropdownMenu.style.display = 'block';
+        } else {
+            dropdownMenu.style.display = 'none';
+        }
+    });
+});
